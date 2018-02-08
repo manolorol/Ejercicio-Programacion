c35b52a40efea0831a122fc81acb55331703e8ce
manolorol <manu08950@gmail.com>
2018-02-08 10:19:45 +0100

    Esto es para el ejercicio

---
+++ src/Coche.java
@@ -0,0 +1,20 @@
+
+public class Coche {
+	Integer cilindrada;
+	String color;
+	Double precio;
+	Boolean isArrancado;
+
+	public static void main(String[] args) {}
+		// TODO Apéndice de método generado automáticamente
+		
+		void arrancar() {
+			if (isArrancado !=true) {
+				isArrancado =true;
+				System.out.println("¡¡¡Brrrrr!!! El coche acaba de arrancar.");
+			} else {
+				System.out.println("¡¡¡Kisch, kisch!!! El coche ya está arrancando.");
+	}
+		}
+		}
+
